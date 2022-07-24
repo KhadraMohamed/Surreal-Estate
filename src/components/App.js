ed0d1dac99b634aad49f7c98a14164d673a2bc09
khadra mohamed <khadramohamed@live.co.uk>
2022-07-24 19:36:56 +0100

    Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "../styles/app.css";
+import { Route, Switch } from "react-router-dom";
 import Navbar from "./NavBar";
+import Properties from "./Properties";
+import AddProperty from "./AddProperty";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <Switch>
+        <Route exact path="/" component={Properties} />
+        <Route exact path="/add-property" component={AddProperty} />
+      </Switch>
       <header className="App-header">
         <h2> Surreal Estate </h2>
       </header>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/navbar.css";
+import { Link } from "react-router-dom";
 import image from "../image/logo.png";
 
 const NavBar = () => {
@@ -7,8 +8,12 @@ const NavBar = () => {
     <div className="navbar">
       <img className="logo" src={image} alt="navbarlogo" />
       <ul className="navbar-links">
-        <li className="navbar-links-item"> View Properties </li>
-        <li className="navbar-links-item"> Add a Property </li>
+        <Link className="navbar-link-item" to="/">
+          View Properties
+        </Link>
+        <Link className="navbar-link-item" to="/add-property">
+          Add a Property
+        </Link>
       </ul>
     </div>
   );
