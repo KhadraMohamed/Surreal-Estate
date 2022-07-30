03a40fb8fa6f7820313042ed60d038cf218cab0c
khadra mohamed <khadramohamed@live.co.uk>
2022-07-30 17:29:47 +0100

    Properties

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,5 +1,35 @@
-import React from "react";
+import axios from "axios";
+import { React, useEffect, useState } from "react";
+import Alert from "./Alert";
+import PropertyCard from "./PropertyCard";
+import "../styles/properties.css";
 
-const Properties = () => <div> Properties Page </div>;
+const Properties = () => {
+  const [properties, setProperties] = useState([]);
+  const [alert, setAlert] = useState("");
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/v1/PropertyListing/`)
+      .then(({ data }) => setProperties(data))
+      .catch(() => {
+        setAlert({
+          message: "Server error. Please try again later.",
+        });
+      }, []);
+    return (
+      <div>
+        <div> Properties Page </div>;
+        <div className="properties">
+          {properties.map((property) => (
+            <div key={property._id} className="item">
+              <PropertyCard {...property} />
+            </div>
+          ))}
+        </div>
+        <Alert message={alert.message} success={alert.isSuccess} />
+      </div>
+    );
+  });
+};
 
 export default Properties;
